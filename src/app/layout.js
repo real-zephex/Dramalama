fcaee47f9790c377737dbf9a429da988068dcb15
real-zephex <zephex-1@outlook.com>
2024-03-17 09:31:49 +0530

    Added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./header/header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +15,8 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
+				<SpeedInsights />
+				<Analytics />
 				<Header />
 				{children}
 			</body>
