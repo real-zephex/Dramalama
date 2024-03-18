b1f4f412059760d85f531c3d6e2ee626c9002d28
real-zephex <zephex-1@outlook.com>
2024-03-18 14:03:32 +0530

    fixes: change the file extension to .jsx and fixed the video player sizes for tablets

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'asianimg.pro',
-          pathname: '/cover/**',
-        },
-        {
-          protocol: "https",
-          hostname: "www.pngall.com"
-        },
-        {
-          protocol: "https",
-          hostname: "gogocdn.net"
-        }
-      ],
-    },
-  };
-  
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "asianimg.pro",
+				pathname: "/cover/**",
+			},
+			{
+				protocol: "https",
+				hostname: "www.pngall.com",
+			},
+			{
+				protocol: "https",
+				hostname: "gogocdn.net",
+			},
+		],
+	},
+};
+
 export default nextConfig;
-  
