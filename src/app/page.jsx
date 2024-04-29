c60af963c720a71bd129e2bbe6af3d2b3775e4fc
zephex-alt <166333351+zephex-alt@users.noreply.github.com>
2024-04-29 04:30:06 +0000

    adjusted the width and height of the image just by a little

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,16 +24,6 @@ export default function Home() {
 							<p>Your one stop for all your kdrama needs</p>
 						</Link>
 					</div>
-					<div className={styles.netlify}>
-						<Link
-							href={"https://dramalama.netlify.app"}
-							target="_blank"
-							title="Use this if you are experiencing any issues with image loading and stuff like that. Vercel has some known limitations for image loading and other stuff and I seem to run out of them before a month ends so the images end up not loading. Sorry for the inconvience."
-						>
-							<h2>Netlify Instance</h2>
-							<p>comparitively slow but better</p>
-						</Link>
-					</div>
 				</div>
 			</div>
 		</main>
