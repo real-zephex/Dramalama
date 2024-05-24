31c8da42dcf905c9fd9eafcf41cff30c0f988333
real-zephex <zephex-1@outlook.com>
2024-05-24 23:24:27 +0530

    ✅ fix(anime): fix continue watching functionality and minor tweaks

--- src/app/anime/continueWatching/page.jsx
+++ src/app/anime/continueWatching/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import NextImage from "next/image";
+import { Card, CardFooter, Image, Button, Link } from "@nextui-org/react";
 
 const ContinueWatching = () => {
 	const [localItems, setLocalItems] = useState(null);
@@ -23,33 +23,35 @@ const ContinueWatching = () => {
 	}
 
 	return (
-		<main className={styles.main}>
-			<p className={styles.mainText}>Continue Watching</p>
+		<main className="pt-12">
+			<p className="text-sky-400 text-2xl uppercase">Continue Watching</p>
 			{localItems && (
-				<div className={styles.animeContainer}>
+				<div className="flex flex-col m-auto lg:w-[95%] ">
 					{localItems.watchHis &&
 						localItems.watchHis.map((item, index) => (
 							<Link
 								href={`/${item.type}/${item.id}`}
-								style={{ textDecoration: "none" }}
 								key={index}
+								color="foreground"
+								className="mt-4 bg-gray-300 p-2 dark:bg-[#1f1f1f] rounded-lg"
 							>
-								<div className={styles.animeEntry}>
-									<div className={styles.titleContainer}>
-										<h3>{item.name}</h3>
-										<p className={styles.EpisodeCount}>
-											Episode watching: {item.episode}
+								<div className="flex items-center justify-between w-full">
+									<div>
+										<p className="font-bold text-xl lg:text-2xl w-3/5">
+											{item.name}
 										</p>
-										<p className={styles.date}>
-											Last watched on: {item.date} at{" "}
-											{item.time} hours
-										</p>
+										<p>Episode: {item.episode}</p>
 									</div>
 									<Image
+										as={NextImage}
+										isZoomed
+										isBlurred
+										shadow="lg"
 										src={item.image}
-										width={167}
-										height={267}
+										width={180}
+										height={300}
 										alt="Continue anime poster"
+										className="h-52 w-auto lg:h-64 lg:w-52"
 										priority
 									/>
 								</div>
--- src/app/components/workInProgress/page.jsx
+++ src/app/components/workInProgress/page.jsx
@@ -1,7 +1,7 @@
 const WorkInProgress = async () => {
 	return (
 		<main className="h-screen w-screen flex flex-col items-center justify-center">
-			<p className="text-sky-400 uppercase">
+			<p className="text-sky-400 uppercase text-center">
 				This section is undergoing a complete overhaul. Sorry for the
 				inconvenience.
 			</p>
