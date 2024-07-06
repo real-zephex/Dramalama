5bffecdcc752f64c721d318cfc3733001a41eee8
real-zephex <zephex-1@outlook.com>
2024-07-06 11:35:02 +0530

    feat?: addded google analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -53,6 +53,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className={lexend.className}>
+			<head>
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-64S37Q7YY4"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-64S37Q7YY4');
+					`,
+					}}
+				></script>
+			</head>
+
 			<body>
 				<Providers>
 					<Header />
